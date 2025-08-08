784aae5e4f30eeeb419c362223f991c8cd723287
xandersavage <47666196+xandersavage@users.noreply.github.com>
2025-08-08 23:34:59 +0100

    Update Features and TypewriterEffect components: modify text content for clarity and adjust styling for improved readability

--- src/components/ui/typewriter-effect.tsx
+++ src/components/ui/typewriter-effect.tsx
@@ -72,7 +72,7 @@ export const TypewriterEffect = ({
   return (
     <div
       className={cn(
-        "text-base sm:text-xl md:text-3xl lg:text-5xl font-bold text-center",
+        "font-bold text-center dark:text-white text-black text-2xl leading-snug tracking-wide",
         className
       )}
     >
